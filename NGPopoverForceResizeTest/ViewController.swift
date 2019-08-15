f139b707e2637d5b058f79db5588018d04bf580f
Noah Gilmore <noah.gilmore@autodesk.com>
2019-08-15 16:21:51 -0500

    Initial Commit

---
+++ NGPopoverForceResizeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NGPopoverForceResizeTest
+//
+//  Created by Noah Gilmore on 8/15/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
